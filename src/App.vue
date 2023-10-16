c898bb15c07760e804712091228d01171936b1a0
muteeburrehman <muteeb285@gmail.com>
2023-10-16 20:14:27 +0500

    using transitions

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,60 @@
 <template>
   <div class="container">
-    <div class="block" :class="{animate:animatedBlock}"></div>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition name="para">
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <!-- There is one exception in transition element and it is that when we have two 
+  if statements and we guarentee there is one child element active in a dom at a time -->
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
     return {
       animatedBlock: false,
-      dialogIsVisible: false 
+      dialogIsVisible: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
     hideDialog() {
       this.dialogIsVisible = false;
     },
-    animateBlock(){
-       this.animatedBlock=true;
-    }
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
   },
 };
 </script>
@@ -63,7 +88,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  /*   */
+  /* transition: transform 0.3s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -77,19 +102,68 @@ button:active {
   border-radius: 12px;
 }
 .animate {
+  /* transform: translateX(-150px); */
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
-  0%{
-    transform: translate(0) scale(1);
-  }
-  70% {
-    transform: translate(-120px) scale(1.1);
-  }
-  100% {
-    transform:translate(-150px) scale(1)
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.para-enter-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.para-leave-active {
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
+}
+
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to{
+  opacity: 0;
+}
+
+.fade-button-enter-active{
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active{
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from{
+  opacity: 1;
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
   }
 
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
